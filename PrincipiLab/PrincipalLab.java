6a5ea9905fd8970616198019e72658dc65ced729
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-05 23:26:42 -0600

    Principal

--- PrincipiLab/PrincipalLab.java
+++ PrincipiLab/PrincipalLab.java
@@ -1,22 +1,22 @@
 /*
 Esta va a ser nuestra clase principal para el ejemplo de
-separación de clases
+separacion de clases
 */ 
 
-clase pública PrincipalLab{
-    //metodo director
+public class PrincipalLab{
+    //metodo principal
     public static void main(String[] args){
         //y ahorita no le pondremos nada de nada
         /*
         Para poder hacer uso de los metodos de la segunda clase
- es necesario primero el crear un objeto para invocar la invocación
+        es necesario primero el crear un objeto para invocacion
         de sus metodos u operaciones, y asi
         poder acceder a sus atributos 
         
         */
 
-        Ejercicios obj - nuevos Ejercicios();
-        //invocación
- obj. menu();
+        Ejercicios obj = new Ejercicios();
+        //invocacion
+        obj.menu();
     }
 }

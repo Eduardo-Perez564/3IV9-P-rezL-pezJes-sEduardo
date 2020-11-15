97bd327e20c2de9d657dfc6009dbb326b18a6f20
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-14 18:03:43 -0600

    Ejercicios

---
+++ MetodosPrimeraParte/Ejercicios.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Ejercicios{
+
+    Scanner entrada = new Scanner(System.in);
+
+    char opc;
+    
+    public void menu(){
+
+        JOptionPane.showMessageDialog(null, "Bienvenido");
+    }
+
+    
+
+}

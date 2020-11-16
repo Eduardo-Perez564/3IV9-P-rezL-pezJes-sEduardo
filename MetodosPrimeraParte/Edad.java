1559ec89118c8692cb724447bdef703da1082d33
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-15 21:56:47 -0600

    Ejercicios

--- MetodosPrimeraParte/Edad.java
+++ MetodosPrimeraParte/Edad.java
@@ -1,49 +1,39 @@
 import java.util.Scanner;
 
-
 public class Edad{
+    
+    public void perdir_datos(){
+    
+        Scanner entrada = new Scanner(System.in);
 
-    Scanner entrada = new Scanner(System.in);
+        int mes, anio;
+        int edada, edadm;
 
-    char opc, re;
+        System.out.println("En que mes naciste (introducelo como numero, por favor)?");
+        mes = entrada.nextInt();
+        System.out.println("En que anio naciste (introducelo como numero, por favor)?");
+        anio = entrada.nextInt();
 
-    public class Edad1{
-        public void perdir_datos(){
-        
-            Scanner entrada = new Scanner(System.in);
-            
-            int mes, anio;
-            int edada, edadm;
-    
-            System.out.println("En que mes naciste (introducelo como numero, por favor)?");
-            mes = entrada.nextInt();
-            System.out.println("En que anio naciste (introducelo como numero, por favor)?");
-            anio = entrada.nextInt();
-    
-            edada = calcular_edada(anio);
-    
-            edadm = calcular_edada(mes);
-    
-            System.out.println("Tu edad es de"+edada+"anios y "+edadm+"meses");
-    
-        }
-    
-        public int calcular_edada(int anio){
-    
-            int aa;
-            aa = 2020-anio;
-    
-            return aa;
-        }
-        public int calcular_edadm(int mes){
-    
-            int mm;
-            mm = 12-mes;
-            
-            return mm;
-        }
+        edada = calcular_edada(anio);
+
+        edadm = calcular_edada(mes);
+
+        System.out.println("Tu edad es de"+edada+"anios y "+edadm+"meses");
+
     }
 
-    
-    
+    public int calcular_edada(int anio){
+
+        int aa;
+        aa = 2020-anio;
+
+        return aa;
+    }
+    public int calcular_edadm(int mes){
+
+        int mm;
+        mm = 12-mes;
+        
+        return mm;
+    }
 }

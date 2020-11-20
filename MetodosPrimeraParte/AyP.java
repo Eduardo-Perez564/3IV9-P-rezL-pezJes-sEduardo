47157c870d162f63fde55d64aa18e06ed53af2b9
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-20 16:06:57 -0600

    Figuritas uwu

---
+++ MetodosPrimeraParte/AyP.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AyP{
+
+    public void menu(){
+
+        Scanner entrada = new Scanner(System.in);
+
+        double radio, apo, Afig, Pfig;
+        int lados, opc;
+
+        System.out.println("Que quieres calcular?");
+        System.out.println();
+        System.out.println("1. Area y Perimetro de un circulo");
+        System.out.println("2. Area y Perimetro de un rectangulo");
+        System.out.println("3. Area y Perimetro de un cuadrado");
+        System.out.println("4. Area y Perimetro de un triangulo");
+
+        opc = entrada.nextInt();
+
+    }
+
+}

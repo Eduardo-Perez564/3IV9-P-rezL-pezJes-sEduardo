b0136c605fde78723e692d21e8f637b3e8605c1c
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-22 22:40:07 -0600

    Arreglito uwu

--- Arreglos/Programas.java
+++ Arreglos/Programas.java
@@ -2,6 +2,51 @@ import java.util.scanner;
 
 public class Programas{
 
-    public void 
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        char opc, exit;
+        
+        do{
+            System.out.println("Bienvenido");
+            System.out.println();
+            System.out.println("Que programa deseas ejecutar?");
+            System.out.println();
+            System.out.println("A. Promedio de 10 numeros.");
+            System.out.println("B. Numeros enteros.");
+            System.out.println("C. Calificaciones.");
+            System.out.println("D. Matrices.");
+
+            opc = entrada.next().charAt(0);
+
+            switch(opc){
+                case 'A':
+                case 'a':
+
+                    break;
+                /*case 'B':
+                case 'b':
+                
+                    break;
+                case 'C':
+                case 'c':
+                
+                    break;
+                case 'D':
+                case 'd':
+                
+                    break; */
+                default:
+                    break;
+
+            }
+
+        }while();
+        
+
+
+
+    }
 
 }

e6ca29c5b02c92d9fe2aaa3ff68d7b0a079ccc54
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-23 02:44:01 -0600

    Sueldo

---
+++ MetodosSegundaParte/Sueldo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sueldo{
+
+    Scanner entrada = new Scanner(System.in);
+    
+    public void dinerito(){
+
+        int id_emple
+        String nombre;
+
+    }
+
+}

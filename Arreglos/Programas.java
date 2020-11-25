9b0ce8c3710553f3c8deb3dacb777c180273b623
Jesus Perez <jperezl1902@alumno.ipn.mx>
2020-11-25 03:33:08 -0600

    AyP

--- Arreglos/Programas.java
+++ Arreglos/Programas.java
@@ -44,8 +44,23 @@ public class Programas{
 
         }while();
         
+        public void Promedio(){
 
+            int promedio [] = new int [3];
+            int num;
+            float promediop, promedion;
+            
+            System.out.println("En este programa lo que puedes hacer es insertar 10 numeros, ya sean positivos o negativos");
+            System.out.println("y se te devolvera el promedio de los números positivos y el promedio de los valores negativos.");
 
+            for (int i; i<promedio.length; i++){
+
+                System.out.println("Introduce el número");
+                promedio [i] = entrada.nextInt();
+            }
+
+            
+        }           
 
     }
 
--- MetodosPrimeraParte/AyP.java
+++ MetodosPrimeraParte/AyP.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class AyP{
 
+    Scanner entrada = new Scanner(System.in);
+
     public void menu(){
 
-        Scanner entrada = new Scanner(System.in);
-
-        double radio, apo, Afig, Pfig;
         int lados, opc;
 
         System.out.println("Que quieres calcular?");
@@ -17,6 +16,51 @@ public class AyP{
         System.out.println("4. Area y Perimetro de un triangulo");
 
         opc = entrada.nextInt();
+
+        switch (opc) {
+            case 1:
+                AyP obj = new AyP();
+                obj.circulo();
+                break;
+        
+            default:
+
+                System.out.println("Lo siento no existe tal opción O.o");
+                break;
+        }
+
+    }
+
+    public void circulo(){
+
+        double radio;
+        double cir, area;
+
+        System.out.println("Ingresa el tamaño del radio");
+        radio = entrada.nextFloat();
+
+        cir = calcularcir(radio);
+        area = calcular_area(radio);
+
+        System.out.println("La circunferencia de tu circulo es "+cir+" y el area es"+area);
+
+    }
+
+    public double calcularcir(double radio){
+
+        double cir;
+        cir = 3.1416*(2*radio);
+
+        return cir;
+
+    }
+
+    public double calcular_area(double radio){
+
+        double area;
+        area = 3.1416*(radio*radio);
+
+        return area;
 
     }
 
